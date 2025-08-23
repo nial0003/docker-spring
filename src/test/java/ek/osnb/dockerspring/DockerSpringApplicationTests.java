5f5e9d941f37258855bbdb3ffe5b1bfeaa0bbc9e
osman-butt <butt.osman@gmail.com>
2025-08-23 15:44:34 +0200

    Spring init

---
+++ src/test/java/ek/osnb/dockerspring/DockerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ek.osnb.dockerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
