2b14da6106d9a0919aa980ceac8a8f3c9dd3fd03
osman-butt <butt.osman@gmail.com>
2025-09-01 23:46:42 +0200

    Updated controller

--- src/main/java/ek/osnb/dockerspring/controller/MessageController.java
+++ src/main/java/ek/osnb/dockerspring/controller/MessageController.java
@@ -4,11 +4,13 @@ import ek.osnb.dockerspring.model.Message;
 import ek.osnb.dockerspring.repository.MessageRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/messages")
 public class MessageController {
 
     private final MessageRepository messageRepository;
